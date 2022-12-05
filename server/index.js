248f37dc81dc07aac8411fbc31cedbb4dadba6d8
Amit Mahato <amit643.mahato@gmail.com>
2022-12-05 23:25:05 +0545

    feat: use message signature to generate sender address for transfer of amount to provided recepient

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { generateAddress, recoverPublicKeyFromSignature } = require("./utils");
+
 const port = 3042;
 
 app.use(cors());
@@ -9,9 +11,9 @@ app.use(express.json());
 // key - wallet address
 // value - balance for corresponding wallet address
 const balances = {
-  "0x1": 100,
+  "0x0495951b865b38ece48d5c9a4d2d26e8ef5ee218": 100,
   "0xf3bb44837de36076123386a46db8a64f8e491e82": 50,
-  "0x3": 75,
+  "0xc216c3cddc4a01f8f6222bb4cb82f8dd7fad83d5": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -21,17 +23,34 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  try {
+    const { message, signature, recoveryBit } = req.body;
+    const { recipient, amount } = message;
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+    // The order in which original object data was placed before using JSON.stringify should not alter
+    // otherwise the hash message generated will be completely different than it was originally generated
+    // at client side, that is why it is best to send the data used to generate the hash be sent in its own object
+    const publicKey = recoverPublicKeyFromSignature(
+      message,
+      signature,
+      recoveryBit
+    );
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+    const sender = generateAddress(publicKey);
+
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
+
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
   }
 });
 
